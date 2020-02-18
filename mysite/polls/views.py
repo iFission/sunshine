5472b70a100ab783ad9457df41173ca9dc43b78b
iFission <alexwwj@icloud.com>
2020-02-18 15:06:26 +0800

    feat: complete part 4 views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 
 def index(request):
@@ -16,8 +17,43 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {questio}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        # request.POST = dict object
+        # request.POST['key'] to extract the value, always string
+        # raises KeyError if key not found
+        # request.GET similar
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            'polls/detail.html',
+            {
+                'question': question,
+                'error_message': "You did not select a choice"
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(
+            reverse('polls:results', args=(question.id, )))
+        # 'polls:results' to reference the url
+        # input args, to simulate
+
+        # HttpResponseRedirect(url to redirect to)
+        # rather than normal
+        # convention to do redirect after POST
+
+        # reverse(name_of_view, variable of the url to the view)
+        # eg reverse('polls:results', args=(question.id,))
+        # => '/polls/3/results/'
+        # similar to {% url 'polls:detail' question.id %}
+
+        # overall redirect to /polls/3/results/
