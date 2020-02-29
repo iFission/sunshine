e80a7d97d5ce2370044c0993b4420ec9d61789ef
alicekham <50978017+alicekham@users.noreply.github.com>
2020-02-29 19:09:46 +0800

    feat: website w/o node-module folder

---
+++ Website/src/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Contact = () => (
+    <div>
+    <h2>Contact</h2>    
+    <p> Spicy jalapeno bacon ipsum dolor amet tenderloin chuck spare ribs pig fatback. Bresaola shoulder chislic shank pastrami kielbasa. T-bone shoulder venison pork belly doner meatball frankfurter landjaeger. Chislic short ribs fatback leberkas ground round drumstick jowl prosciutto.
+        Spare ribs chicken pork loin shank turkey, kielbasa prosciutto. Fatback tail corned beef swine frankfurter strip steak. Pork chop pork ball tip ham hock, meatball beef ribs short ribs pork belly fatback. Hamburger tongue ball tip swine jowl meatball ham pork loin short ribs bacon venison pork ground round brisket kielbasa. Swine boudin meatloaf t-bone chicken, picanha tail shoulder spare ribs flank burgdoggen sirloin chislic leberkas. Short loin tenderloin jowl, cupim biltong chuck jerky. Bacon jerky pig, picanha shoulder bresaola ball tip.
+        Tongue hamburger cupim buffalo pork belly ham. Alcatra ball tip meatball salami. Andouille boudin cow bresaola hamburger tongue jerky leberkas buffalo. Shankle filet mignon pastrami meatball. Short ribs leberkas prosciutto shankle jerky salami brisket.
+        Spare ribs pancetta venison chicken doner kevin short ribs biltong chislic filet mignon chuck andouille. T-bone ham hock prosciutto, ground round pork belly shoulder jowl capicola ribeye kevin pig short loin alcatra tail. Burgdoggen tail hamburger salami pastrami meatloaf flank. Pastrami tongue kevin venison picanha shank corned beef leberkas tail salami buffalo frankfurter capicola swine meatball. Tri-tip pork belly pastrami beef short ribs chicken spare ribs chislic pork bacon jowl landjaeger corned beef jerky sausage. Short loin turducken ribeye flank t-bone landjaeger drumstick ham bacon frankfurter jowl tenderloin ball tip fatback beef.
+    </p>    
+    </div>
+      
+)
---
+++ Website/src/components/Jumbotron.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Jumbotron as Jumbo, Container } from 'react-bootstrap'
+import styled from 'styled-components';
+import leaf from '../assets/leaf.jpg'
+
+const Styles = styled.div`
+
+.jumbo {
+    background: url(${leaf}) no-repeat fixed bottom;
+    background-size: cover;
+    color: white;
+    height: 300px;
+    position: relative;
+    z-index: -2;
+}
+.overlay {
+    background-color: #000;
+    opacity: 0.6;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+}
+`;
+
+export const Jumbotron = () => (
+    <Styles>
+    <Jumbo fluid className = 'jumbo'>
+        <div className='overlay'></div>
+        <Container>
+            <h1>Welcome</h1>
+            <p>Learn how to code yay</p>
+        </Container>   
+    </Jumbo>
+    </Styles>
+)
