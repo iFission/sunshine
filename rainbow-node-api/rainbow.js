e7c12a47fc75bc616a2b9b3b2afe3addccc56048
iFission <alexwwj@icloud.com>
2020-03-13 10:43:51 +0800

    feat: create user account

--- rainbow-node-api/rainbow.js
+++ rainbow-node-api/rainbow.js
@@ -98,4 +98,27 @@ let rainbowSDK = new RainbowSDK(options);
 rainbowSDK.start().then(() => {
   // Do something when the SDK is connected to Rainbow
   console.log("DEBUG: services started");
+  let userEmailAccount = "john.d122oe@myCompany.com";
+  let userPassword = "bestpassworD1!";
+  let userFirstname = "John";
+  let userLastname = "Doe";
+
+  rainbowSDK.admin
+    .createUserInCompany(
+      userEmailAccount,
+      userPassword,
+      userFirstname,
+      userLastname
+    )
+    .then(user => {
+      // Do something when the user has been created and added to that company
+      console.log(`DEBUG: user created`);
+      console.log(`DEBUG: ${userEmailAccount}`);
+      console.log(`DEBUG: ${userPassword}`);
+      console.log(`DEBUG: ${user.id}`);
+    })
+    .catch(err => {
+      // Do something in case of error
+      console.log(`DEBUG: user failed to create`);
+    });
 });
