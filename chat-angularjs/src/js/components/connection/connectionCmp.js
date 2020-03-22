3265cff64025361429ad797a0b51fab3964bca36
Etzernal <chan.jielin@dhs.sg>
2020-03-23 03:05:33 +0800

    feat: adds auto login, connect network

--- chat-angularjs/src/js/components/connection/connectionCmp.js
+++ chat-angularjs/src/js/components/connection/connectionCmp.js
@@ -3,65 +3,32 @@ angular.module("sample").component("rbxConnection", {
     name: "@"
   },
   controller: function rbcConnectionCtrl(rainbowSDK, $rootScope, $scope) {
-    $scope.isConnected = false;
-
-    $scope.isLoading = false;
-
     $scope.state = rainbowSDK.connection.getState();
 
-    $scope.hosts = [
-      {
-        id: 0,
-        value: "sandbox",
-        name: "Rainbow Sandbox"
-      },
-      {
-        id: 1,
-        value: "rainbow",
-        name: "Rainbow Official"
-      }
-    ];
-
-    $scope.selectedItem = $scope.hosts[0];
+    console.log("[DEMO] :: HELLO");
+    console.log($scope.state);
+    $scope.isConnected = false;
+    $scope.isLoading = false;
 
     var handlers = [];
 
-    $scope.signin = function() {
+    var signin = function(userid,userpass) {
       $scope.isLoading = true;
-
-      saveToStorage();
-
-      switch ($scope.selectedItem.value) {
-        case "rainbow":
-          rainbowSDK.connection
-            .signinOnRainbowOfficial($scope.user.name, $scope.user.password)
-            .then(function(account) {
-              console.log("[DEMO] :: Successfully signed!");
-              $scope.isLoading = false;
-              $scope.isConnected = true;
-            })
-            .catch(function(err) {
-              console.log("[DEMO] :: Error when sign-in", err);
-              $scope.isLoading = false;
-              $scope.isConnected = false;
-            });
-          break;
-        default:
-          rainbowSDK.connection
-            .signin($scope.user.name, $scope.user.password)
-            // .signin("jielin_chan@mymail.sutd.edu.sg","1pvQ$9G=Bo}1")
-            .then(function(account) {
-              console.log("[DEMO] :: Successfully signed!");
-              $scope.isLoading = false;
-              $scope.isConnected = true;
-            })
-            .catch(function(err) {
-              console.log("[DEMO] :: Error when sign-in", err);
-              $scope.isLoading = false;
-              $scope.isConnected = false;
-            });
-          break;
-      }
+      console.log("[DEMO] :: HELLO");
+      rainbowSDK.connection
+        .signin(userid,userpass)
+        .then(function(account) {
+          console.log("[DEMO] :: Successful login!");
+          $scope.isLoading = false;
+          $scope.isConnected = true;
+          addContact(agentid);
+        })
+        .catch(function(err) {
+          console.log("[DEMO] :: Error when login", err);
+          $scope.isLoading = false;
+          $scope.isConnected = false;
+        });
+      console.log("[DEMO] :: HELLO3");
     };
 
     $scope.signout = function() {
@@ -72,30 +39,66 @@ angular.module("sample").component("rbxConnection", {
       });
     };
 
-    var saveToStorage = function() {
-      sessionStorage.connection = angular.toJson($scope.user);
-      sessionStorage.host = angular.toJson($scope.selectedItem);
-    };
+    var addContact = function(agentid) {
+      console.log("After user is signed in, add agent contact to user's network");
+      var selectedContact = null;
+      // Check connection status - make sure it is connected 
+      console.log("Connection Status:");
+      console.log(rainbowSDK.connection.getState());
+      // Search for Contact object of agent, using agent id. 
+      rainbowSDK.contacts
+        .searchById(agentid)
+        .then(function(entityFound) {
+            console.log("Adding to network");
+            if (entityFound) {
+              // Contact object found - entityFound, add agent to user's network. 
+              return rainbowSDK.contacts.addToNetwork(entityFound);
+            }
+        })
+        .then(function() {
+            // Do something once the invitation has been sent.
+            console.log("Invitation succesfully sent to",agentid);
+            // Open the conversation
+        })
+        .catch(function(err) {
+            // Not connected yet or Add to network failed. 
+            console.log("Invitation failed",err);
+        });
+    }
 
-    var readFromStorage = function() {
-      if (sessionStorage.connection) {
-        $scope.user = angular.fromJson(sessionStorage.connection);
-      } else {
-        $scope.user = { name: "", password: "" };
-      }
+    var ContactReturn = function(contactId) {
+        var selectedContact =null;
+        // Contact not found locally, ask to the server
+        console.log("Connection Status:");
+        console.log(rainbowSDK.connection.getState());
+        if(!selectedContact) {
+            rainbowSDK.contacts.searchById(contactId).then(function(contact) {
+                selectedContact = contact;
+                if(selectedContact) {
+                    // Ok, we have the contact object
+                    console.log("Got it");
+                    console.log(selectedContact.lastname);
+                    return selectedContact;
+                }
+                else {
+                    // Strange, no contact with that Id. Are you sure that the id is correct?
+                    console.log("No contact found on server");
+                }
 
-      if (sessionStorage.host) {
-        $scope.selectedItem =
-          $scope.hosts[angular.fromJson(sessionStorage.host).id];
-      } else {
-        $scope.selectedItem = $scope.hosts[0];
-      }
-    };
+            }).catch(function(err) {
+                //Something when wrong with the server. Handle the trouble here
+                console.log("Failed to find",err);
+            });;
+        } 
+    }
 
     var onConnectionStateChangeEvent = function onConnectionStateChangeEvent(
       event
     ) {
       $scope.state = rainbowSDK.connection.getState();
+      console.log("Hello:");
+      console.log($scope.state);
+
     };
 
     this.$onInit = function() {
@@ -116,11 +119,15 @@ angular.module("sample").component("rbxConnection", {
       }
     };
 
-    var initialize = function() {
-      readFromStorage();
-    };
-
-    initialize();
+    // Get agent and user info from database
+    var agentid = "5e6af1a2566b33069dadefd8";
+    // Create user 
+    var userid = 'potato@company.com';
+    var userpass = 'Abcd@123';
+    signin(userid,userpass);
+    console.log("HELLOWORLD");
+    // Connect agent and user 
+    // addContact(agentid);
   },
   templateUrl: "./src/js/components/connection/connectionCmp.template.html"
 });
